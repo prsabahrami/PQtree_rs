a98abce3007cb33f0e01b500cb94c75d3fb15853
prsabahrami <pbahrami@uwaterloo.ca>
2024-07-26 17:01:00 -0400

    Basic setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
 use pyo3::prelude::*;
+use pyo3::types::PyList;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+#[pyclass]
+struct PQ {
+    children: Vec<PyObject>,
 }
 
-/// A Python module implemented in Rust.
+#[pymethods]
+impl PQ {
+    #[new]
+    fn new(seq: Bound<'_, PyList>) -> PyResult<Self> {
+        let children = seq.iter().map(|x| x.to_object(seq.py())).collect();
+        Ok(PQ { children })
+    }
+}
+
+#[pyclass(extends=PQ)]
+struct P {}
+
+#[pymethods]
+impl P {
+    #[new]
+    fn new(seq: Bound<'_, PyList>) -> PyResult<(Self, PQ)> {
+        let pq = PQ::new(seq)?;
+        Ok((P {}, pq))
+    }
+
+}
+
+#[pyclass(extends=PQ)]
+struct Q {}
+
+#[pymethods]
+impl Q {
+    #[new]
+    fn new(seq: Bound<'_, PyList>) -> PyResult<(Self, PQ)> {
+        let pq = PQ::new(seq)?;
+        Ok((Q {}, pq))
+    }
+
+}
+
 #[pymodule]
-fn pqtree_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn pq_trees(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<PQ>()?;
+    m.add_class::<P>()?;
+    m.add_class::<Q>()?;
     Ok(())
 }
